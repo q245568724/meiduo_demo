6debb8fd04bac473775e8fa31ae5a72eadb3594f
xwp <xwp_fullstack@163.com>
2019-02-19 12:09:50 +0800

    前端get请求

--- mall/apps/users/views.py
+++ mall/apps/users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User
+
+"""
+前端发送用户给后端 我们后端判断用户名 是否注册
+请求方式
+GET   /users/usernames/(?P<username>\w{5,20})/count/
+
+POST
+"""
+
+class RegisterUsernameAPIView(APIView):
+
+    def get(self,request,username):
+        # 判断用户是否注册
+        # 查询用户名的数量,0就是没有注册,1就是有注册
+        count = User.objects.filter(username=username).count()
+
+        return Response({'count':count})
