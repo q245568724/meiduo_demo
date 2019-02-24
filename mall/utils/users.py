fdbdd4616dc4d04b6ef67a466d847f596450c187
xwp <xwp_fullstack@163.com>
2019-02-24 09:43:36 +0800

    多张好登陆中将用户验证抽取

--- mall/utils/users.py
+++ mall/utils/users.py
@@ -13,6 +13,19 @@ def jwt_response_payload_handler(token, user=None, request=None):
         'username':user.username
     }
 
+def get_user_by_account(username):
+    try:
+        if re.match(r'1[3-9]\d{9}', username):
+            # 手机号
+            user = User.objects.get(mobile=username)
+        else:
+            # 用户名
+            user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        user = None
+
+    return user
+
 from django.contrib.auth.backends import ModelBackend
 
 class UsernameMobleModelBackend(ModelBackend):
@@ -20,15 +33,16 @@ class UsernameMobleModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
 
         #1 根据用户名确认用户输入的是手机号还是用户名
-        try:
-            if re.match(r'1[3-9]\d{9}',username):
-                # 手机号
-                user = User.objects.get(mobile=username)
-            else:
-                # 用户名
-                user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            user = None
+        # try:
+        #     if re.match(r'1[3-9]\d{9}',username):
+        #         # 手机号
+        #         user = User.objects.get(mobile=username)
+        #     else:
+        #         # 用户名
+        #         user = User.objects.get(username=username)
+        # except User.DoesNotExist:
+        #     user = None
+        user = get_user_by_account(username)
         #2 验证用户密码
         if user is not None and user.check_password(password):
             return user
